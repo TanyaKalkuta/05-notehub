934479c28ac506b5c4c697c4db930bfaff5d4bbc
Tetiana Kalkuta <72383361+TanyaKalkuta@users.noreply.github.com>
2025-11-24 20:00:53 +0000

    created notes- app

---
+++ src/components/App/App.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import css from "./App.module.css";
+import NoteList from "../NoteList/NoteList";
+import Pagination from "../Pagination/Pagination";
+import SearchBox from "../SearchBox/SearchBox ";
+import Modal from "../Modal/Modal";
+import NoteForm from "../NoteForm/NoteForm";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import { toast, Toaster } from "react-hot-toast";
+import { useDebounce } from "use-debounce";
+import fetchNotes from "../../services/noteService";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+
+function App() {
+  const [search, setSearch] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [debounceValue] = useDebounce(search, 1000);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  const { data, isLoading, isFetching, isError, isSuccess } = useQuery({
+    queryKey: ["notes", debounceValue, currentPage],
+    queryFn: () =>
+      fetchNotes({
+        page: currentPage,
+        perPage: 10,
+        search: debounceValue,
+      }),
+    placeholderData: keepPreviousData,
+  });
+  useEffect(() => {
+    if (isSuccess && debounceValue.trim() !== "" && data?.notes.length === 0) {
+      toast.error("No notes found for your request.");
+    }
+  }, [isSuccess, data, debounceValue]);
+
+  return (
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        {<SearchBox search={search} onChange={setSearch} />}
+        {(isLoading || isFetching) && <Loader />}
+        {isError && <ErrorMessage />}
+
+        {data && data.totalPages > 1 && (
+          <Pagination
+            totalPages={data.totalPages}
+            currentPage={currentPage}
+            onPageChange={setCurrentPage}
+          />
+        )}
+        <button className={css.button} onClick={openModal}>
+          Create note +
+        </button>
+      </header>
+      {isError && <p>Error loading notes</p>}
+      {isLoading && <p>Loading...</p>}
+      {data && <NoteList notes={data.notes} />}
+
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <NoteForm onClose={closeModal} />
+      </Modal>
+      <Toaster position="top-center" reverseOrder={false} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,32 @@
+import css from "./Modal.module.css";
+import { createPortal } from "react-dom";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+export default function Modal({ isOpen, onClose, children }: ModalProps) {
+  if (!isOpen) return null;
+
+  const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  return createPortal(
+    <div
+      className={css.backdrop}
+      role="dialog"
+      aria-modal="true"
+      onClick={handleBackdropClick}
+    >
+      <div className={css.modal} onClick={(e) => e.stopPropagation()}>
+        {children}
+      </div>
+    </div>,
+    document.body
+  );
+}
